3a88f66871f0b70084a1e47e9081e3e117eca17f
Raheem <akapsyjr@gmail.com>
2025-09-18 00:13:51 +0100

    shifted the app.appcontext()

--- main.py
+++ main.py
@@ -349,9 +349,8 @@ def history():
         for m in messages
     ])
 
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
